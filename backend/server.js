9e6ab718ca14e32c295630193e8f80f98abc5b6c
Kubeva <kubeva171@gmail.com>
2026-01-24 17:34:04 +0100

    Added database modifications

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,25 @@
 import express from "express";
 import cors from "cors";
 import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const dbPath = path.join(__dirname, "list_data.json");
+
 app.get("/database", (req, res) => {
-    const data = JSON.parse(fs.readFileSync("list_data.json"));
+    const data = JSON.parse(fs.readFileSync(dbPath));
     res.json(data);
 });
 
 app.post("/database", (req, res) => {
     const newData = req.body;
-    fs.writeFileSync("list_data.json", JSON.stringify(newData, null, 2));
+    fs.writeFileSync(dbPath, JSON.stringify(newData, null, 2));
     res.json({ message: "Saved!" });
 });
 
--- src/JS/DatabaseEditor.jsx
+++ src/JS/DatabaseEditor.jsx
@@ -1,53 +1,225 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import '../CSS/DatabaseEditor.css'
 
 function DatabaseEditor() {
   const [database, setDatabase] = useState([]);
+  const [loading, setLoading] = useState(true);
   const [newAttribute, setNewAttribute] = useState("");
+  const [deleteAttribute, setDeleteAttribute] = useState("");
+  const [newType, setNewType] = useState(""); 
+  const [formAddError, setFormAddError] = useState("");
+  const [formDeleteError, setFormDeleteError] = useState("");
 
   const databaseAttributes = database.length > 0 ? Object.keys(database[0]) : [];
 
-  const addNewAttribute = (e) => {
+  const addNewAttribute = async (e) => {
     e.preventDefault()
 
     const attrName = newAttribute.trim();
-    if(!attrName) return;
+    if(!attrName) {
+      setFormAddError("Attribute name is blank")
+      return;
+    }
+    if(!newType) {
+      setFormAddError("Wrong type")
+      return;
+    }
+
+    setFormAddError("");
+
+    if(database.length > 0 && attrName in database[0]) {
+      alert("Attribute already exists");
+      return;
+    }
+
+    let defaultValue = getFormDefaultValueType(newType);
 
     const updatedDatabase = database.map((item) => ({
       ...item,
-      [attrName]: ""
+      [attrName]: defaultValue
     }));
 
-    setDatabase(updatedDatabase)
-    setNewAttribute("")
+    try {
+      const res = await fetch("http://localhost:4000/database", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(updatedDatabase)
+      });
+
+      if(!res.ok){
+        throw new Error("Failed to save JSON.");
+      }
+
+      console.log("Saved JSON file.");
+      setDatabase(updatedDatabase);
+      setNewAttribute("");
+    } catch(err) {
+      console.error(err);
+      alert(err);
+    }
   };
+
+  const deleteExistingAttribute = async (e) => {
+    e.preventDefault()
+
+    const attrName = deleteAttribute.trim();
+    if(!attrName){
+      setFormDeleteError("Attribute name is blank");
+      return;
+    }
+
+    setFormAddError("");
+
+    if(database.length > 0 && !(attrName in database[0])) {
+      alert(`Attribute "${attrName}" doesn't exist`);
+      return;
+    }
+
+    const updatedDatabase = database.map(item => {
+      const newItem = { ...item };
+      delete newItem[attrName];
+      return newItem;
+    });
+
+    try {
+      const res = await fetch("http://localhost:4000/database", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(updatedDatabase)
+      });
+
+      if(!res.ok){
+        throw new Error(`Failed to delete "${attrName}"`);
+      }
+
+      console.log(`Deleted "${attrName}" in JSON file.`);
+      setDatabase(updatedDatabase);
+      setDeleteAttribute("");
+    } catch(err) {
+      console.error(err);
+      alert(err);
+    }
+  };
+
+  const getFormDefaultValueType = (type) => {
+    switch(type) {
+      case "string": return "";
+      case "number": return 0;
+      case "boolean": return false;
+      case "array": return [];
+      case "object": return {};
+    }
+  }
+
+  const fetchDatabase = async () => {
+    try{
+      const res = await fetch("http://localhost:4000/database");
+      const data = await res.json();
+      setDatabase(data);
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  const getType = (value) => {
+    if(Array.isArray(value)) return "array";
+    return typeof value;
+  }
+
+  useEffect(() => {
+    fetchDatabase();
+  }, []);
 
   return (
     <>
       <div className="container p-3">
         <h1>Database Editor</h1>
-        <div className="container">
+        <div className="container p-2">
           <h2>Example</h2>
-          {database.length === 0 ? (
-            <p>No elements in JSON</p>
-          ) : (
-            <div>
-              {databaseAttributes.map((attribute) =>(
-                <p key={attribute}>{attribute}: {database[0][attribute]?.toString() || "N/A"}</p>
-              ))}
+          {loading ? (
+            <div class="spinner-border spinner-color" role="status">
+              <span class="visually-hidden">Loading...</span>
             </div>
+          ) : database.length === 0 ? (
+            <p className="text-danger">No elements in JSON</p>
+          ) : (
+            <table className="mt-4">
+              <thead>
+                <tr>
+                  <th className="pe-3">Attribute</th>
+                  <th className="pe-3">Example value</th>
+                  <th>Value type</th>
+                </tr>
+              </thead>
+              <tbody>
+                {databaseAttributes.map((attribute) =>(
+                  <tr key={attribute}>
+                    <td className="fw-bold pe-3">{attribute}</td>
+                    <td className="pe-3">{database[0][attribute]?.toString() || "N/A"}</td>
+                    <td>{getType(database[0][attribute])}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
           )}
         </div>
-        <div className="container">
-          <h2>Add Attribute To JSON</h2>
-          <form onSubmit={addNewAttribute}>
-            <div className="form-group">
-              <label className="form-label">New Attribute Name</label>
-              <input className="form-control" type="text" value={newAttribute} onChange={(e) => setNewAttribute(e.target.value)}></input>
+        <div className="container p-2">
+          <h2>Modify Database</h2>
+          <form onSubmit={addNewAttribute} className="mt-4">
+            <div className="row g-3 align-items-end">
+              <div className="col-md-5">
+                <label className="form-label">Add New Attribute</label>
+                <input 
+                  className="form-control attribute-input" 
+                  type="text" 
+                  value={newAttribute} 
+                  placeholder="Enter attribute name"
+                  onChange={(e) => setNewAttribute(e.target.value)} />
+              </div>
+              <div className="col-md-2">
+                <label className="form-label"></label>
+                <select className="form-select attribute-input" value={newType} onChange={(e) => setNewType(e.target.value)}>
+                  <option value="">Choose type</option>
+                  <option value="string">String</option>
+                  <option value="number">Number</option>
+                  <option value="boolean">Boolean</option>
+                  <option value="array">Array</option>
+                  <option value="object">Object</option>
+                </select>
+              </div>
+              <div className="col-md-2">
+                <button className="btn btn-primary attribute-button" type="submit">
+                  Add attribute
+                </button>
+              </div>
             </div>
-            <button className="btn btn-primary" type="submit">
-              Add attribute
-            </button>
           </form>
+          {formAddError && <div className="text-danger mt-2">{formAddError}</div>}
+          <form onSubmit={deleteExistingAttribute} className="mt-4">
+            <div className="row g-3 align-items-end">
+              <div className="col-md-5">
+                <label className="form-label">Delete Attribute</label>
+                <input 
+                  className="form-control attribute-input" 
+                  type="text" 
+                  value={deleteAttribute} 
+                  placeholder="Enter attribute name"
+                  onChange={(e) => setDeleteAttribute(e.target.value)} />
+              </div>
+              <div className="col-md-2">
+                <button className="btn btn-danger" type="submit">
+                  Delete attribute
+                </button>
+              </div>
+            </div>
+          </form>
+          {formDeleteError && <div className="text-danger mt-2">{formDeleteError}</div>}
         </div>
       </div>
     </>
